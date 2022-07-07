da11b6d1c5d6d30532e3e08c7e24866877410424
Anmol Gupta <guptaanmol6264@gmail.com>
2022-07-07 14:39:32 +0530

    Time: 5 ms (31.82%), Space: 6.8 MB (55.01%) - LeetHub

---
+++ 884-uncommon-words-from-two-sentences/884-uncommon-words-from-two-sentences.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> uncommonFromSentences(string s1, string s2) {
+        vector<string> ans;
+        map<string,int> mp;
+        string str = "";
+        for(int i=0;i<s1.length();i++){
+            if(s1[i]==' '){
+                mp[str]++;
+                str ="";
+            }
+            else str+=s1[i];
+        }
+        mp[str]++; str = "";
+        for(int j=0;j<s2.length();j++){
+            if(s2[j]==' '){
+                mp[str]++;
+                str ="";
+            }
+            else str+=s2[j];
+        }
+        mp[str]++;
+        for(auto i:mp){
+            if(i.second==1) ans.push_back(i.first);
+        }
+        return ans;
+    }
+};
