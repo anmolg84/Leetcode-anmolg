2929fa2d3f7ece6b04a6b251b41896a1fe26fdc0
Anmol Gupta <guptaanmol6264@gmail.com>
2022-07-07 15:33:07 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (74.49%) - LeetHub

--- 461-hamming-distance/461-hamming-distance.cpp
+++ 461-hamming-distance/461-hamming-distance.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     int hammingDistance(int x, int y) {
+        // Approach 1
+        // int ans = 0;
+        // for(int i=0;i<32;i++){
+        //     if(((x & (1<<i)) && !(y & (1<<i))) || (!(x & (1<<i)) && (y & (1<<i)))){
+        //         ans++;
+        //     } 
+        // }
+        // return ans;
+        // Approach 2
         int ans = 0;
+        int value = x^y;
         for(int i=0;i<32;i++){
-            if(((x & (1<<i)) && !(y & (1<<i))) || (!(x & (1<<i)) && (y & (1<<i)))){
-                ans++;
-            } 
+            if(value & (1<<i)) ans++;
         }
         return ans;
     }
