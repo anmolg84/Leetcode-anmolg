2a8b98b96f175a071132003add718bf921a0aebe
Anmol Gupta <guptaanmol6264@gmail.com>
2022-07-08 16:50:08 +0530

    Time: 236 ms (5.99%), Space: 45.1 MB (22.74%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int ans = 0;
-        int sum = 0;
+        // Approach 1
+        // int ans = 0;
+        // int sum = 0;
+        // int n = nums.size();
+        // map<int,int> mp;
+        // for(int i=0;i<n;i++){
+        //     sum+=nums[i];
+        //     if(sum==k) ans++;
+        //     if(mp.find(sum-k)!=mp.end()){
+        //         ans+=(mp[sum-k]);
+        //     }
+        //     mp[sum]++;
+        // }
+        // return ans;
+        // Approach 2
         int n = nums.size();
-        map<int,int> mp;
+        vector<int> prefix(n);
+        prefix[0] = nums[0];
+        for(int i=1;i<n;i++) prefix[i] = nums[i]+prefix[i-1];
+        map<int,int> mp; int ans = 0;
         for(int i=0;i<n;i++){
-            sum+=nums[i];
-            if(sum==k) ans++;
-            if(mp.find(sum-k)!=mp.end()){
-                ans+=(mp[sum-k]);
+            if(prefix[i]==k) ans++;
+            if(mp.find(prefix[i]-k)!=mp.end()){
+                ans+=mp[prefix[i]-k];
             }
-            mp[sum]++;
+            mp[prefix[i]]++;
         }
         return ans;
     }
