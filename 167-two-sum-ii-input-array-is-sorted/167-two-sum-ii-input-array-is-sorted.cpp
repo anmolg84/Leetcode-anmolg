c71cf96181d040eef8daf0048c536d7da8fc182b
Anmol Gupta <guptaanmol6264@gmail.com>
2022-07-08 16:23:00 +0530

    Time: 25 ms (30.15%), Space: 15.5 MB (75.35%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        vector<int> ans;
-        unordered_map<int,int> mp;
+        // Approach 1
+        // vector<int> ans;
+        // unordered_map<int,int> mp;
+        // int n = numbers.size();
+        // for(int i=0;i<n;i++){
+        //     if(mp.find(target-numbers[i])!=mp.end()){
+        //         ans.push_back(i+1); ans.push_back(mp[target-numbers[i]]+1);
+        //         sort(ans.begin(),ans.end());
+        //         return ans;
+        //     }
+        //     else{
+        //         mp[numbers[i]]=i;
+        //     }
+        // }
+        // return ans;
+        // Approach 2
         int n = numbers.size();
-        for(int i=0;i<n;i++){
-            if(mp.find(target-numbers[i])!=mp.end()){
-                ans.push_back(i+1); ans.push_back(mp[target-numbers[i]]+1);
-                sort(ans.begin(),ans.end());
-                return ans;
-            }
-            else{
-                mp[numbers[i]]=i;
-            }
+        int low = 0;
+        int high = n-1;
+        while(low<high){
+            if(numbers[low]+numbers[high]<target) low++;
+            else if(numbers[low]+numbers[high]>target) high--;
+            else return vector<int>{low+1,high+1};
         }
-        return ans;
+        return vector<int>{};
     }
 };
