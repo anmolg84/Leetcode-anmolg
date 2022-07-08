c886d6274a3e857a1f654fd44588b821d87a48cf
Anmol Gupta <guptaanmol6264@gmail.com>
2022-07-08 19:40:32 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (74.75%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        // // Approach 1
+        // Approach 1
         // return a+b;
-        // // Approach 2 (if a and b both are positive)
+        // Approach 2 (if a and b both are positive)
         // for(int i=1;i<=b;i++){
         //     a+=1;
         // }
         // return a;
-        // Approach 3 (if a and b both are positive)
+        // Approach 3 
         while(b){
             unsigned carry = a & b;
             a = a ^ b;
             b = carry << 1;
+            cout << a << b << " ";
         }
         return a;
     }
